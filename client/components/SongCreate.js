4ce87c3c86f95cf3722235499896a87f1debe8f9
Damien Nelson <dhnisaman@gmail.com>
2023-12-02 16:31:25 -0500

    update mutation to refetch query and navigate

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link, hashHistory } from 'react-router'
+import query from '../queries/FetchSongs'
 
 class SongCreate extends Component {
     constructor(props) {
@@ -11,23 +13,24 @@ class SongCreate extends Component {
 
     onSubmit(event){
         event.preventDefault()
-
         this.props.mutate({
             variables: {
                 title: this.state.title
-            }
-        })
+            },
+            refetchQueries: [{ query }]
+        }).then(() => hashHistory.push('/') )
     }
 
     render() {
         return (
-        <div>
-            <h3>Create a new song</h3>
-            <form onSubmit={this.onSubmit.bind(this)}>
-                <label>Song Title:</label>
-                <input value={this.state.title} onChange={event => this.setState({title: event.target.value})} />
-            </form>
-        </div>
+            <div>
+                <Link to="/">Back</Link>
+                <h3>Create a new song</h3>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title:</label>
+                    <input value={this.state.title} onChange={event => this.setState({title: event.target.value})} />
+                </form>
+            </div>
         )
     }
 }
