fcd903c6a89c2175b97487b41a51d9fa836bbcf6
Damien Nelson <dhnisaman@gmail.com>
2023-12-06 23:27:32 -0500

    import LyricList and render to SongDetail page

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router'
 import { graphql } from 'react-apollo'
 import fetchSong from '../queries/FetchSong'
 import LyricCreate from './LyricCreate'
+import LyricList from './LyricList'
 
 
 class SongDetail extends Component {
@@ -15,6 +16,7 @@ class SongDetail extends Component {
             <div>
                 <Link to='/'>Back</Link>
                 <h3>{song.title}</h3>
+                <LyricList lyrics={song.lyrics} />
                 <LyricCreate songId={this.props.params.id}/>
             </div>
         )
