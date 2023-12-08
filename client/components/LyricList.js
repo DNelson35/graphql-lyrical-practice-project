a62c75b74c1a0c6c95c7e4b985fb894e7d7ddd77
Damien Nelson <dhnisaman@gmail.com>
2023-12-07 23:58:20 -0500

    update likes to use optimistic updates

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -4,8 +4,18 @@ import { graphql } from 'react-apollo'
 
  class LyricList extends Component {
 
-  onLike(id) {
-    this.props.mutate({ variables: { id } })
+  onLike(id, likes) {
+    this.props.mutate({ 
+      variables: { id }, 
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
+    })
   }
     renderLyrics(){
       return this.props.lyrics.map(({id, content, likes }) => {
@@ -13,7 +23,7 @@ import { graphql } from 'react-apollo'
               <li key={id} className='collection-item'>
                   {content}
                  <div className='vote-box'>
-                    <i className='material-icons' onClick={() => this.onLike(id)}>thumb_up</i>
+                    <i className='material-icons' onClick={() => this.onLike(id, likes)}>thumb_up</i>
                     {likes}
                  </div>
               </li>
